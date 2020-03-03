cab467552301308c87fc9c9c69525bb28504cfef
Cosmina Palade <cosmina.palade@gmail.com>
2020-03-03 22:46:58 +0200

    remove links

--- docs/index.js
+++ docs/index.js
@@ -254,7 +254,8 @@ const lis = document.querySelectorAll("li");
 const a = document.querySelectorAll("li a");
 
 for (let i = 0; i < lis.length; i++) {
-    lis[i].addEventListener("click", function () {
+    lis[i].addEventListener("click", function (e) {
+        e.preventDefault();
         for (let i = 0; i < lis.length; i++) {
             lis[i].classList.remove("active");
             a[i].classList.remove("active-text");
