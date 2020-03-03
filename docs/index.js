6a4ae16969117e5825c53bbaa21624949d86fa5e
Cosmina Palade <cosmina.palade@gmail.com>
2020-03-03 21:27:07 +0200

    styling the buttons

--- docs/index.js
+++ docs/index.js
@@ -3,7 +3,7 @@ let $root = $('html, body');
 // Major sections
 let $machine = $('.machine');
 let $shelves = $('.shelves');
-let $selection = $('.selection');
+let $selection = $('.selection > ul > li');
 let $coins = $('.coins');
 let $form = $('form');
 let $input_selection = $form.find('[name="selection"]');
@@ -248,3 +248,18 @@ $form.submit(function (e) {
         }, 10);
     }
 });
+
+// Buttons animation
+const lis = document.querySelectorAll("li");
+const a = document.querySelectorAll("li a");
+
+for (let i = 0; i < lis.length; i++) {
+    lis[i].addEventListener("click", function () {
+        for (let i = 0; i < lis.length; i++) {
+            lis[i].classList.remove("active");
+            a[i].classList.remove("active-text");
+        }
+        this.classList.add("active");
+        a[i].classList.add("active-text");
+    });
+}
